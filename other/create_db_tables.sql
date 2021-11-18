12e1c091d2d28a6dfea792d9db0374cbcb773cea
Ham_sandwich <arttimon28@mail.ru>
2021-11-18 13:57:41 +0500

    Ввёл регистрацию-авторизацию Решил проблему с хранением хэша. Пользователь может входить и выходить из своей записи - по сути входить и выходить из сессии

---
+++ other/create_db_tables.sql
@@ -0,0 +1,35 @@
+CREATE TABLE users (
+	id_user SERIAL,
+	email VARCHAR(40) NOT NULL,
+	pass_hash VARCHAR(100) NOT NULL,
+	lastname VARCHAR(20) NOT NULL,
+	firstname VARCHAR(20) NOT NULL,
+	avatar BYTEA,
+	CONSTRAINT user_pkey PRIMARY KEY(id_user),
+	CONSTRAINT user_unique_email UNIQUE(email),
+	CONSTRAINT user_check_strings CHECK(
+		(email != '') AND (firstname != '') AND (lastname != '')
+	)
+);
+
+CREATE TABLE conferences (
+	id_conf SERIAL,
+	title VARCHAR(60) NOT NULL,
+	description TEXT,
+	time_conf TIMESTAMP NOT NULL,
+	period_conf INTERVAL,
+	id_creator INTEGER,
+	is_active BOOLEAN,
+	CONSTRAINT conf_pkey PRIMARY KEY(id_conf),
+	CONSTRAINT conf_check_strings CHECK(
+		(title != '')
+	)
+);
+
+CREATE TABLE user_conf (
+	id_user INTEGER NOT NULL,
+	id_conf INTEGER NOT NULL,
+	in_conf BOOLEAN DEFAULT FALSE NOT NULL,
+	FOREIGN KEY(id_user) REFERENCES users(id_user) ON DELETE CASCADE,
+	FOREIGN KEY(id_conf) REFERENCES conferences(id_conf) ON DELETE CASCADE
+);
